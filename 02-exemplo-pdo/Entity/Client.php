92c435f0c2a33811d4cf36b90c84086efa1dd5b2
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-14 12:21:00 -0300

    add: Adicionado ID para entidade Client

--- 02-exemplo-pdo/Entity/Client.php
+++ 02-exemplo-pdo/Entity/Client.php
@@ -4,12 +4,14 @@ namespace Entity;
 
 class Client {
 
+  private int $id;
   private string $name;
   private string $email;
 
-  public function __construct(string $name, string $email) {
+  public function __construct(string $name, string $email, int $id) {
     $this->setName($name);
     $this->setEmail($email);
+    $this->setId($id);
   }
   
   public function getName(): string {
@@ -26,6 +28,14 @@ class Client {
 
   public function setEmail(string $email): void {
     $this->email = $email;
+  }
+
+  public function getId(): int {
+    return $this->id;
+  }
+
+  private function setId(int $id): void {
+    $this->id = $id;
   }
 
   public function __toString(): string {
