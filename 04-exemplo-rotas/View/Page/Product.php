3e2249daeb92a4294928a9dff9bce80f35f3485c
Daniel Rocha <rochadaniel15@gmail.com>
2021-06-23 13:00:31 -0300

    add: Adicionado exemplo de view com tabela

--- 04-exemplo-rotas/View/Page/Product.php
+++ 04-exemplo-rotas/View/Page/Product.php
@@ -3,12 +3,23 @@
 
 <h1>Produtos - File</h1>
 <ul>
-<?php
-  
-  foreach ($products as $value) {
-    echo "<li>${value}</li>";
-  }
-?>
+<table>
+  <tr>
+    <td>Name</td>
+    <td>Price</td>
+  </tr>
+  <?php foreach ($products as $value): ?>
+    <tr>
+      <td><?php printf("${value['name']}") ?></td>
+      <td><?php printf("%.2f", $value['price'])  ?></td>
+    </tr>
+  <?php endforeach?>
+  <style>
+    table, td {
+      border:1px solid #000;
+    }
+  </style>
+</table>
 
 </ul>
 <?php include __DIR__ . "/../Component/Footer.php";?>
